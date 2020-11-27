83fca3fe22e3c955712c35869fd2f8c0589c2ab2
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2020-11-27 03:38:10 +0300

    flamethrower

---
+++ Assets/Scripts/Controller/DroneEnemyController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class DroneEnemyController : MonoBehaviour
+{
+    private PlayerParametersModel _player;
+    private Animator _animator;
+}
---
+++ Assets/Scripts/Model/DroneEnemyModel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class DroneEnemyModel : MonoBehaviour
+{
+    public bool IsDead;
+
+    [SerializeField] private float _currentHP;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _attackPower;
+    [SerializeField] private float _attackRange;
+    
+
+    public void GetDamage(float damage)
+    {
+        _currentHP -= damage;
+
+        if (_currentHP <= 0)
+        {
+            _currentHP = 0;
+            IsDead = true;
+        }
+    }
+    public float GetSpeed()
+    {
+        return _speed;
+    }
+    public float GetCurrentHP()
+    {
+        return _currentHP;
+    }
+    public float GetAttackPower()
+    {
+        return _attackPower;
+    }
+    public float GetAttackRange()
+    {
+        return _attackRange;
+    }
+}
